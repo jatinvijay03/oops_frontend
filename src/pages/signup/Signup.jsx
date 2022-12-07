14a56da6bdc17b9f8d50103259ab61d9d8016f9c
Shubham <shubham@Shubhams-MacBook.local>
2022-12-07 05:58:22 +0530

    amazing work

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -203,11 +203,11 @@ export default function Signup() {
                 <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                     <div className="w-full max-w-md space-y-8">
                         <div>
-                            {/* <img
-                                className="mx-auto h-12 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            <img
+                                className="mx-auto h-20 w-auto agglog"
+                                src="https://i.ibb.co/rMxVn9q/Screen-Shot-2022-12-07-at-3-22-06-AM-removebg-preview.png"
                                 alt="Your Company"
-                            /> */}
+                            />
                             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                                 Create Your Account
                             </h2>
